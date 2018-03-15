070969b884f93bfd7b57bdd2bbc6216fd723ba5f
Paul Baker <paul.baker@wgu.edu>
2018-03-15 13:53:35 -0600

    Better output. Days left until payday is the exit code

--- main.go
+++ main.go
@@ -13,14 +13,14 @@ func main() {
 	// See this for example (be sure to update the second date):
 	// https://www.timeanddate.com/date/durationresult.html?m1=1&d1=5&y1=2018&m2=3&d2=15&y2=2018
 	howManyDays := currentDateRange.Days() - 1
-	currentDayInPayPeriod := howManyDays % 14
-	fmt.Printf("%d days until we get paid.", 14-currentDayInPayPeriod)
-	if currentDayInPayPeriod == 0 {
-		fmt.Print(" PAYDAY!! :D")
+	daysUntilPayDay := 14 - howManyDays%14
+	fmt.Printf("%d day(s) until we get paid.", daysUntilPayDay)
+	if daysUntilPayDay == 0 {
+		fmt.Print(" IT'S PAYDAY!! :D")
 	}
 	fmt.Println()
 	// Only returns success (0) when it is payday
 	// Useful for shell scripts and automation that
 	// should be run only on this day.
-	os.Exit(currentDayInPayPeriod)
+	os.Exit(daysUntilPayDay)
 }
