d46e90e3eb5a7a10e38977633d8f41a9dfa25aa7
Paul Baker <paul.baker@wgu.edu>
2018-03-15 14:21:19 -0600

    Line break

--- example.sh
+++ example.sh
@@ -4,7 +4,8 @@
 check-payday > /dev/null 2>&1
 days_left="${?}"
 if [ "${days_left}" -eq 0 ]; then
-  echo "It is indeed PAYDAY! Time to use automation to move money into savings, pay bills, and use AI to gamble (EG: buy bitcoin/nasdaq)."
+  echo "It is indeed PAYDAY!"
+  echo "Time to use automation to move money into savings, pay bills, and use AI to gamble (EG: buy bitcoin/nasdaq)."
 else
   echo "We have ${days_left} days(s) until payday. Let's stop execution here."
   exit 1
