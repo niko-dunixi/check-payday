eb25a7f7beef394518d940ec4ca50ef0e8bb9017
Paul Baker <paul.baker@wgu.edu>
2018-03-16 00:58:14 -0600

    Parameterizable now.
    We can set a custom base payday to make calculations off of. Using mine as default so I don't need to specify anything extra :P

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"os"
 	"fmt"
 	"github.com/paul-nelson-baker/check-payday/util"
+	"github.com/jessevdk/go-flags"
 )
 
+type options struct {
+	DateString string `short:"d" long:"date" description:"A given date that is a payday, all calculations will be based on this date. Must be in format YYYY-MM-DD with leading 0's. (First payday of the year is advisable, but the most recent will suffice.)"`
+}
+
 func main() {
-	firstPayDate := date.New(2018, 1, 5)
+	firstPayDate := getDate()
 	daysUntilPayDay, isPayDay := util.CalculatePayDayInfo(firstPayDate, date.Today())
 	if !isPayDay {
 		var suffix string
@@ -25,4 +30,22 @@ func main() {
 	// Useful for shell scripts and automation that
 	// should be run only on this day.
 	os.Exit(daysUntilPayDay)
+}
+
+func getDate() date.Date {
+	options := options{}
+	_, err := flags.Parse(&options)
+	if err != nil {
+		os.Exit(-1)
+	}
+	// If no one has specified a pay-day, it's probably me using the tool.
+	// Set the default to my first payday of 2018. Deal wit' it, yo!
+	if options.DateString == "" {
+		options.DateString = "2018-01-05"
+	}
+	parsedDate, err := date.Parse(options.DateString)
+	if err != nil {
+		panic(err)
+	}
+	return parsedDate
 }
